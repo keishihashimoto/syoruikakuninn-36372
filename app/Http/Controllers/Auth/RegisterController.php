2e1486d9a8a8f54ac5ce38f5c313c90b66aac7de
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-20 23:03:57 +0900

    コントローラの修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -29,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/user_licenses/create";
 
     /**
      * Create a new controller instance.
@@ -53,6 +54,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            "year" => ["required"],
+            "month" => ["required"],
+            "date" => ["required"]
         ]);
     }
 
@@ -64,10 +68,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            "birthday" => "{$data['year']}-{$data['month']}-{$data['date']}"
         ]);
     }
 }
