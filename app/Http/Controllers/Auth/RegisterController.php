0f4fb8cd5eb92987e88a260d403322cfe57ef98f
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-21 18:20:21 +0900

    ユーザ登録時のバリデーションを修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,9 +54,32 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            "year" => ["required"],
-            "month" => ["required"],
-            "date" => ["required"]
+            "year" => ["required", "integer", "min:1800"],
+            "month" => ["required", "integer", "min:1", "max:12"],
+            "date" => ["required", "integer", "min:1", "max:31"]
+        ],[
+            "name.required" => "お名前を入力してください",
+            "name.string" => "お名前に不適切な文字が使用されています",
+            "name.max" => "お名前は255文字以下で入力してください",
+            "email.required" => "メールアドレスを入力してください",
+            "email.string" => "メールアドレスに不適切な文字が使用されています",
+            "email.max" => "メールアドレスは255文字以下で入力してください",
+            "email.unique" => "このメールアドレスはすでに使用されています",
+            "password.required" => "パスワードが入力されていません",
+            "password.string" => "パスワードに不適切な文字が使用されています",
+            "password.min" => "パスワードには最低でも8文字以上が必要です",
+            "password.confirmed" => "パスワードが一致しません",
+            "year.required" => "お生まれになった年が入力されていません",
+            "year.integer" => "お生まれになった年は数字で入力してください",
+            "year.min" => "お生まれになった年は4桁の数字で入力してください",
+            "month.required" => "お生まれになった月が入力されていません",
+            "month.integer" => "お生まれになった月は数字で入力してください",
+            "month.min" => "お生まれになった月は1 ~ 12で入力してください",
+            "month.max" => "お生まれになった月は1 ~ 12で入力してください",
+            " date.required" => "お生まれになった日が入力されていません",
+            "date.integer" => "お生まれになった日は数字で入力してください",
+            "date.min" => "お生まれになった日は1 ~ 31で入力してください",
+            "date.max" => "お生まれになった日は1 ~ 31で入力してください"
         ]);
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">お名前</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,25 +26,58 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Birthday') }}<i> (全て半角数字で入力してください)</i></label>
+                            <label for="birthday" class="col-md-4 col-form-label text-md-right">ご生年月日<i> (全て半角数字で入力してください)</i></label>
 
                             <div class="col-md-6">
-                                <input id="year" type="text" class="form-control @error('year') is-invalid @enderror" name="year" value="{{ old('year') }}" required autocomplete="year" placeholder="西暦で入力してください" autofocus>年
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <input id="year" type="text" class="form-control @error('year') is-invalid @enderror" name="year" value="{{ old('year') }}" required autocomplete="year" placeholder="西暦で入力してください" autofocus>
+                                    <div class="p-2">年</div>
+                                </div>
 
-                                <input id="month" type="text" class="form-control @error('month') is-invalid @enderror" name="month" value="{{ old('month') }}" required autocomplete="month" placeholder="半角数字で入力してください" autofocus>月
+                                @if($errors->has("year"))
+                                <div>
+                                    @foreach($errors->get("year") as $error)
+                                    <div style="list-style: none;" role="alert" class="text-danger birthday-errors">
+                                        <strong>{{ $error }}</strong>
+                                    </div>
+                                    @endforeach
+                                </div>   
+                                @endif
 
-                                <input id="date" type="text" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" required autocomplete="date" placeholder="半角数字で入力してください" autofocus>日
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <input id="month" type="text" class="form-control @error('month') is-invalid @enderror" name="month" value="{{ old('month') }}" required autocomplete="month" placeholder="半角数字で入力してください" autofocus>
+                                    <div class="p-2">月</div>
+                                </div>
 
-                                @error('birthday')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                @if($errors->has("month"))
+                                    <div>
+                                        @foreach($errors->get("month") as $error)
+                                        <div style="list-style: none;" role="alert" class="text-danger birthday-errors">
+                                            <strong>{{ $error }}</strong>
+                                        </div>
+                                        @endforeach
+                                    </div>
+                                @endif
+
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <input id="date" type="text" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" required autocomplete="date" placeholder="半角数字で入力してください" autofocus>
+                                    <div class="p-2">日</div>
+                                </div>
+
+                                @if($errors->has("date"))
+                                <div>
+                                    @foreach($errors->get("date") as $error)
+                                    <div style="list-style: none;" role="alert" class="text-danger birthday-errors">
+                                        <strong>{{ $error }}</strong>
+                                    </div>
+                                    @endforeach
+                                </div>
+                                @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -58,7 +91,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -72,7 +105,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワードをもう一度後入力ください</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
