7f02be18cfbc292f40d93c4590fd71160581e2c9
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-09-21 13:06:17 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserLicenseController;
+use App\Http\Controllers\UserPayController;
+use App\Http\Controllers\UserPaperController;
+use Illuminate\Support\Facades\Route;
 
 
 /*
@@ -26,19 +28,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::resource("users", UserController::class);
 
-Route::get("user_licenses/edit", "App\Http\Controllers\UserLicenseController@edit")->name("user_licenses.edit");
-Route::patch("user_licenses/update", "App\Http\Controllers\UserLicenseController@update")->name("user_licenses.update");
-Route::get("user_licenses/create", "App\Http\Controllers\UserLicenseController@create")->name("user_licenses.create");
-Route::post("user_licenses", "App\Http\Controllers\UserLicenseController@store")->name("user_licenses.store");
+Route::get("user_licenses/edit", [UserLicenseController::class, "edit"])->name("user_licenses.edit");
+Route::patch("user_licenses/update", [UserLicenseController::class, "update"])->name("user_licenses.update");
+Route::get("user_licenses/create", [UserLicenseController::class, "create"])->name("user_licenses.create");
+Route::post("user_licenses", [UserLicenseController::class, "store"])->name("user_licenses.store");
 
-Route::get("/user_pays/create", "App\Http\Controllers\UserPayController@create")->name("user_pays.create");
-Route::post("/user_pays", "App\Http\Controllers\UserPayController@store")->name("user_pays.store");
-Route::get("/user_pays/edit", "App\Http\Controllers\UserPayController@edit")->name("user_pays.edit");
-Route::patch("user_pays/update", "App\Http\Controllers\UserPayController@update")->name("user_pays.update");
+Route::get("/user_pays/create", [UserPayController::class, "create"])->name("user_pays.create");
+Route::post("/user_pays", [UserPayController::class, "store"])->name("user_pays.store");
+Route::get("/user_pays/edit", [UserPayController::class, "edit"])->name("user_pays.edit");
+Route::patch("user_pays/update", [UserPayController::class, "update"])->name("user_pays.update");
 
-Route::get("/user_papers/create", "App\Http\Controllers\UserPaperController@create")->name("user_papers.create");
-Route::post("/user_papers", "App\Http\Controllers\UserPaperController@store")->name("user_papers.store");
-Route::get("/user_papers/edit", "App\Http\Controllers\UserPaperController@edit")->name("user_papers.edit");
-Route::patch("/user_papers/update", "App\Http\Controllers\UserPaperController@update")->name("user_papers.update");
+Route::get("/user_papers/create", [UserPaperController::class, "create"])->name("user_papers.create");
+Route::post("/user_papers", [UserPaperController::class, "store"])->name("user_papers.store");
+Route::get("/user_papers/edit", [UserPaperController::class, "edit"])->name("user_papers.edit");
+Route::patch("/user_papers/update", [UserPaperController::class, "update"])->name("user_papers.update");
 
-Route::resource("memos", "App\Http\Controllers\MemoController");
+Route::resource("memos", MemoController::class);
